4d4294c9f8f4bb9966a2a4c03c5c41bcea412bb7
Hoadt02 <dotathoa2002@gmail.com>
2022-09-28 15:22:45 +0700

    chỉnh cái gì đấy

--- fado-angular/src/app/layouts/admin-layout/admin-layout.module.ts
+++ fado-angular/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,7 +5,6 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
-import { IconsComponent } from "../../pages/icons/icons.component";
 import { MapComponent } from "../../pages/map/map.component";
 import { UserComponent } from "../../pages/user/user.component";
 import { TablesComponent } from "../../pages/tables/tables.component";
@@ -14,6 +13,7 @@ import { TypographyComponent } from "../../pages/typography/typography.component
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import {content} from "../../shared/routes/routes";
 import {NotificationsComponent} from "../../pages/notifications/notifications.component";
+import {IconsComponent} from "../../pages/icons/icons.component";
 
 @NgModule({
   imports: [
