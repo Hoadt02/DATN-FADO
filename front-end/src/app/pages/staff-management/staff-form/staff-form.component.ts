93fd5986993b23423dfd90f5f4acb921bf13d3b1
ThieuVinh <vinhtqph13704@fpt.edu.vn>
2022-09-30 22:43:07 +0700

    vinh up

--- front-end/src/app/pages/staff-management/staff-form/staff-form.component.ts
+++ front-end/src/app/pages/staff-management/staff-form/staff-form.component.ts
@@ -1,8 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {StaffService} from "../../../shared/services/api-service-impl/staff.service";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {FormBuilder} from "@angular/forms";
-import {Constants} from "../../../shared/Constants";
+import {StaffService} from '../../../shared/services/api-service-impl/staff.service';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormBuilder} from '@angular/forms';
+import {Constants} from '../../../shared/Constants';
 
 @Component({
   selector: 'app-staff-form',
@@ -54,8 +54,8 @@ export class StaffFormComponent implements OnInit {
   save() {
     console.log(this.formGroup.getRawValue());
     this.formGroup.markAllAsTouched();
-    if (this.formGroup.invalid){
-      console.log(98726272);
+    if (this.formGroup.invalid) {
+      console.log('abcxyz');
       return;
     }
   }
