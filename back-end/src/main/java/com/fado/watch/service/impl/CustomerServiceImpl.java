7077c354330c48a6f0e54ef8c7ca9b2079991a5f
Sonnh13627 <hongson25082002@gmail.com>
2022-10-03 23:08:59 +0700

    soncomit

--- back-end/src/main/java/com/fado/watch/service/impl/CustomerServiceImpl.java
+++ back-end/src/main/java/com/fado/watch/service/impl/CustomerServiceImpl.java
@@ -1,18 +1,23 @@
 package com.fado.watch.service.impl;
 
 import com.fado.watch.entity.Customer;
+import com.fado.watch.entity.Staff;
 import com.fado.watch.exception.UniqueException;
 import com.fado.watch.repository.CustomerRepository;
 import com.fado.watch.service.ICustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+
 @Service
 public class CustomerServiceImpl implements ICustomerService {
 
-   @Autowired
-   CustomerRepository customerRepository;
+private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll() {
@@ -27,11 +32,33 @@ public class CustomerServiceImpl implements ICustomerService {
     @Override
     public Customer create(Customer customer) {
 
+        if (this.customerRepository.findByUsername(customer.getUsername()).isPresent()) {
+            throw new UniqueException("Username đã tồn tại");
+        }
+        if (this.customerRepository.findByPhoneNumber(customer.getPhoneNumber()).isPresent()) {
+            throw new UniqueException("Số điện thoại đã tồn tại");
+        }
+        if (this.customerRepository.findByEmail(customer.getEmail()).isPresent()) {
+            throw new UniqueException("Email đã tồn tại");
+        }
         return this.customerRepository.save(customer);
     }
 
     @Override
     public Customer update(Customer customer) {
+        Customer customerBefore = this.customerRepository.findById(customer.getId()).get();
+        if (this.customerRepository.findByUsername(customer.getUsername()).isPresent()
+                && !Objects.equals(customer.getUsername(), customerBefore.getUsername())) {
+            throw new UniqueException("Username đã tồn tại");
+        }
+        if (this.customerRepository.findByPhoneNumber(customer.getPhoneNumber()).isPresent()
+                && !Objects.equals(customer.getPhoneNumber(), customerBefore.getPhoneNumber())) {
+            throw new UniqueException("Số điện thoại đã tồn tại");
+        }
+        if (this.customerRepository.findByEmail(customer.getEmail()).isPresent()
+                && !Objects.equals(customer.getEmail(), customerBefore.getEmail())) {
+            throw new UniqueException("Email đã tồn tại");
+        }
         return this.customerRepository.save(customer);
     }
 }
