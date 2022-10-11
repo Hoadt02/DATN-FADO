4c596f43c87fb6744275fcfc72495b96ba25d291
Hoadt02 <dotathoa2002@gmail.com>
2022-10-11 16:38:20 +0700

    project FE fixed

---
+++ FE/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from "ngx-toastr";
+
+import { SidebarModule } from './components/sidebar/sidebar.module';
+import { FooterModule } from './components/footer/footer.module';
+import { NavbarModule} from './components/navbar/navbar.module';
+import { FixedPluginModule} from './components/fixedplugin/fixedplugin.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes,{
+      useHash: true
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot(),
+    FooterModule,
+    FixedPluginModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FE/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+
+
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+    {
+      path: '/dashboard',
+      title: 'Dashboard',
+      icon:'nc-bank',
+      class: ''
+    },
+  {
+    path: '/product-management',
+    title: 'Quản lý sản phẩm',
+    icon: 'nc-watch-time',
+    class: ''
+  },
+  {
+    path: '/staff-management',
+    title: 'Quản lý nhân viên',
+    icon: 'nc-single-02',
+    class: ''
+  },
+  {
+    path: "/promotional-management",
+    title: "Quản lý khuyến mại",
+    icon: "nc-box-2",
+    class: ""
+  },
+  {
+    path: '/customer-management',
+    title: 'Quản lý khách hàng',
+    icon: 'nc-single-02',
+    class: ''
+  },
+  {
+    path: '/material-management',
+    title: 'Quản lý chất liệu',
+    icon: 'nc-atom',
+    class: ''
+  },
+  {
+    path: '/origin-management',
+    title: 'Quản lý xuất xứ',
+    icon: 'nc-shop',
+    class: ''
+  },
+  {
+    path: '/category-management',
+    title: 'Quản lý danh mục',
+    icon: 'nc-bullet-list-67',
+    class: ''
+  },
+  {
+    path: '/brand-management',
+    title: 'Quản lý thương hiệu',
+    icon: 'nc-bold',
+    class: ''
+  },
+];
+
+@Component({
+    moduleId: module.id,
+    selector: 'sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+}
